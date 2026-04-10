d78cd8bc91a01851d582a486792eb0eba1f9954b
ashrafkhan025 <ashrafkhan25013@gmail.com>
2026-04-10 11:17:45 +0530

    fixed cookie config for production (sameSite none)

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,22 @@
-export const generateToken = (user, message, statuscode, res)=>{
-    const token = user.generateJsonWebToken();
-    const cookiename=user.role==="Admin" ? "adminToken": "patientToken";
-    res.status(statuscode).cookie(cookiename, token,{
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        
-        httpOnly: true,
+export const generateToken = (user, message, statuscode, res) => {
+  const token = user.generateJsonWebToken();
+  const cookiename = user.role === "Admin" ? "adminToken" : "patientToken";
+
+  res
+    .status(statuscode)
+    .cookie(cookiename, token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict", 
-})
+      sameSite:
+        process.env.NODE_ENV === "production" ? "none" : "lax",  // 🔥 FIX
+    })
     .json({
-        success: true,
-        message,
-        user,
-        token,
+      success: true,
+      message,
+      user,
+      token,
     });
 };
